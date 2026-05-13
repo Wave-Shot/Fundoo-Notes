d6f2daa88bd521a47c11a57adcf9a0bc8fde08f5
Wave-Shot <vsh.2509@gmail.com>
2026-05-13 11:32:12 +0530

    Reset main branch with README only

--- src/main/java/com/fundoonotes/exception/LabelNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.fundoonotes.exception;
-
-public class LabelNotFoundException extends RuntimeException {
-    public LabelNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/fundoonotes/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.fundoonotes.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
